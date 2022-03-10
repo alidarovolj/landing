7a7a316a13b8a95a6b1e807dec036b28b4722ab6
Olzhas <oljasalidarov@gmail.com>
2022-03-10 16:34:08 +0600

    fgh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="home">
-
+    <div class="container mx-auto">
+      <div class="carousel w-full relative">
+        <div class="inner_carousel relative">
+          <div class="slide w-full h-full">
+            <img class="w-full" src="../assets/1.jpg" alt="">
+          </div>
+          <div class="slide w-full h-full">
+            <img class="w-full" src="../assets/1.jpg" alt="">
+          </div>
+          <div class="slide w-full h-full">
+            <img class="w-full" src="../assets/1.jpg" alt="">
+          </div>
+        </div>
+        <p class="arrow_first">></p>
+        <p class="arrow_second">></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'HomeView',
-  components: {
-
-  },
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
