84a46c74d8e64273c466391b3dcacff64e0b1380
Olzhas <oljasalidarov@gmail.com>
2022-03-10 17:05:35 +0600

    et

--- src/assets/jq.js
+++ src/assets/jq.js
@@ -1,5 +1,16 @@
 import $ from 'jquery'
 
 $(document).ready(function () {
-    console.log('roma')
+    $('.slide').each(function () {
+
+    });
+    $('.arrow_second').on('click', function () {
+        if ($('.slide:nth-child(2)').hasClass('active')) {
+            $('.slide:nth-child(3)').css({
+                transform: 'translateX(-200%)'
+            })
+        } else {
+            console.log('asdsadasdsa')
+        }
+    });
 });
